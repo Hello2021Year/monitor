cf9978a3498ab6da2316cfc4f86e41bb2501aaef
Hello2021Year <78727568+Hello2021Year@users.noreply.github.com>
2023-03-06 00:59:19 +0800

    Update monitor_process.sh

--- bin/monitor_process.sh
+++ bin/monitor_process.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
-source BSC_Node.conf
-source common_function.sh
+source ../conf/Test_Monitor.conf
+source ./common_function.sh
 
 # check service exist or not
 # shellcheck disable=SC1072
-if [[ $(ps -ef | grep $PROCESS | wc -l) -lt $($PROCESS_NUM+1) ]]; then
+if [[ $(ps -ef | grep $PROCESS_NAME | wc -l) -lt $PROCESS_NUM+1 ]]; then
    HOST=`hostname`
    IP=`hostname -i`
    HOST_IP="$HOST:$IP"
    SERVICE_NAME="($HOST_IP) $SERVICE_NAME"
-   inform_info="The $SERVICE_NAME running abnormol"
-   send_message LARK $SERVICE_NAME $inform_info $FATAL $UNRESOLVED
+   inform_info="服务$SERVICE_NAME运行异常!"
+   send_message $SEND_MESSAGE_TYPE "$SERVICE_NAME" "$inform_info" $FATAL $UNRESOLVED
    for((i=1;i<=$MAX_TRIES;i++));
    do
-       #/bin/bash /mnt/vdb1/FullNode/service.sh restart
        $RESTART_COMMAND
        if [[ $? -ne 0 ]];then
-          inform_info="The restart command excute failed!!!"
-          send_message LARK $SERVICE_NAME $inform_info $FATAL $ExecFailed
+          inform_info="重启命令$(RESTART_COMMAND)运行失败!"
+          send_message $SEND_MESSAGE_TYPE "$SERVICE_NAME" "$inform_info" $ERROR $EXECFAILED
        fi
-       sleep $(RESTART_INTERVAL)s
-       if [[ $(ps -ef | grep $PROCESS | wc -l) -lt $($PROCESS_NUM) ]]; then
-          inform_info="Restart the $SERVICE_NAME failed $i times!!!"
-          send_message LARK $SERVICE_NAME $inform_info $ERROR $UNRESOLVED
+       sleep $RESTART_INTERVAL
+       if [[ $(ps -ef | grep $PROCESS_NAME | wc -l) -lt $PROCESS_NUM ]]; then
+          inform_info="重启 $SERVICE_NAME 失败$i次!!"
+          send_message $SEND_MESSAGE_TYPE "$SERVICE_NAME" "$inform_info" $ERROR $UNRESOLVED
        else
-          inform_info="$Restart the $SERVICE_NAME  successfully!!!"
-          send_message LARK $SERVICE_NAME $inform_info $INFO $RESOLVED
+          inform_info="重启 $SERVICE_NAME 成功!"
+          send_message $SEND_MESSAGE_TYPE "$SERVICE_NAME" "$inform_info" $INFO $RESOLVED
           break
        fi
    done
 fi
-
